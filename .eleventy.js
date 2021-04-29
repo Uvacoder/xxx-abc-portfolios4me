6cbd701c0e880023e3597b1c77f3cc21516f8c6b
Acampbell <78562675+fu4303@users.noreply.github.com>
2021-04-29 15:27:24 -0400

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,61 @@
+
+const filenamifyUrl = require('filenamify-url');
+
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
+// const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
+
+const shuffle = require('./filters/shuffle.js');
+const htmlmin = require('html-minifier');
+
+require('dotenv').config();
+
+module.exports = (eleventyConfig) => {
+  // Pass through
+  eleventyConfig.addPassthroughCopy('assets');
+
+  // Collections
+  eleventyConfig.addCollection('sites', (collection) => {
+    return collection.getFilteredByGlob('sites/*.md');
+  });
+
+  // Plugins
+  eleventyConfig.addPlugin(rssPlugin);
+
+  // Filters
+  eleventyConfig.addFilter('shuffle', shuffle);
+  eleventyConfig.addFilter('cleanUrl', (str) => {
+    const urlCruft = /http[s]?:\/\/|\/$/gi;
+    return str.replace(urlCruft, '');
+  });
+
+  // Minify
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (outputPath.indexOf('.html') > -1) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+      });
+      return minified;
+    }
+    return content;
+  });
+
+  // Shortcodes
+  eleventyConfig.addShortcode(
+    'cloudinaryUrl',
+    (path, transforms) =>
+      `https://res.cloudinary.com/${
+        process.env.CLOUDINARY_CLOUD_NAME
+      }/image/upload/${transforms}/${filenamifyUrl(path, {
+        replacement: '',
+      })}.png`
+  );
+
+  // Return config settings
+  return {
+    markdownTemplateEngine: 'njk',
+    passthroughFileCopy: true,
+  };
+};
